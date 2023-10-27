4e2645845b20b9875bff4cebd1744e40cc2038d8
aphoteosiz <mm3262348@gmail.com>
2023-10-26 17:02:52 -0700

    mejora modal areas

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { ModalService } from './modal.service';
+import { CustomModalComponent } from './custom-modal/custom-modal.component';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +9,21 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'modal';
+  constructor(public modalService: ModalService) { }
+
+
+  showModal() {
+    console.log("modal");
+
+    this.modalService.showModal();
+  }
+
+  closeModal() {
+
+  }
+
+  openLogin() {
+    this.modalService.openLogin();
+  }
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,34 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './shared/header/header.component';
+import { FooterComponent } from './shared/footer/footer.component';
+import { DashboardComponent } from './pages/dashboard/dashboard.component';
+import { LoginComponent } from './auth/login/login.component';
+import { NavComponent } from './shared/nav/nav.component';
+import { ReactiveFormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+import { CustomModalComponent } from './custom-modal/custom-modal.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    DashboardComponent,
+    LoginComponent,
+    NavComponent,
+    CustomModalComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    ReactiveFormsModule,
+    HttpClientModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
